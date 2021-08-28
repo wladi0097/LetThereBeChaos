f384f8d5a96273053028d427bf7b23be7d66de7e
Justin Schoenberg <jschoenberg.itsb-b2020@fh-salzburg.ac.at>
2021-08-28 18:51:20 +0200

    Adding UI

--- maps/UiController.gd
+++ maps/UiController.gd
@@ -1,11 +1,24 @@
-extends PanelContainer
+extends MarginContainer
 
-onready var baseTower = preload("res://entities/basicTower.tscn")
+onready var baseTower : PackedScene = preload("res://entities/basicTower.tscn")
+onready var multiShotTower : PackedScene = preload("res://entities/multiShotTower.tscn")
+onready var spikeTower : PackedScene = preload("res://entities/spikeTower.tscn")
 onready var rayCast : RayCast = $placementRayCast
 onready var camera : Camera = $Camera
+onready var messageLabel : Label = $CenterContainer/MessageLabel
 
 var hasSelectedTowerPosition = false
 var towerIdToBePlaced : int = Tower.INVALID
+
+class TowerData:
+	var Ressource : PackedScene
+	var Label : Label
+	var Count : int
+	func _init(_towerRessource : PackedScene, _count : int, _towerLabel : Label):
+		self.Ressource = _towerRessource
+		self.Label = _towerLabel
+		self.Count = _count
+		self.Label.text = "Tower Left: %d" % self.Count
 
 const Tower = {
 	INVALID = -1,
@@ -14,10 +27,39 @@ const Tower = {
 	Spike = 2
 }
 
-func getTower(var towerID : int) -> Spatial:
-	return baseTower.instance()
+onready var TowerDict = {
+	Tower.Base     : TowerData.new( baseTower     , 4, get_node("PanelContainer/VBoxContainer/BaseTowerLabel")  ),
+	Tower.MultiShot: TowerData.new( multiShotTower, 3, get_node("PanelContainer/VBoxContainer/MultiTowerLabel") ),
+	Tower.Spike    : TowerData.new( spikeTower    , 6, get_node("PanelContainer/VBoxContainer/SpikeTowerLabel") ),
+}
 
-func _on_placeTowerButton_pressed(towerID): 
+func getTower(var towerID : int) -> TowerData:
+	if towerID == Tower.INVALID:
+		return TowerDict[Tower.Base]
+	return TowerDict[towerID]
+
+func placeTower(towerData: TowerData, location: Vector3):
+	if towerData.Count <= 0:
+		messageLabel.text = "No more towers!"
+		towerIdToBePlaced = Tower.INVALID
+		hasSelectedTowerPosition = false
+		return
+	
+#	var spaceState : PhysicsDirectSpaceState = get_parent().get_world().direct_space_state
+#	var collidingShapes = spaceState.collide_shape()
+	
+	var tower = towerData.Ressource.instance()
+	tower.transform.origin = location
+	get_tree().get_root().add_child(tower)
+
+	towerIdToBePlaced = Tower.INVALID
+	hasSelectedTowerPosition = false
+	messageLabel.text = ""
+	towerData.Count -= 1;
+	towerData.Label.text = "Tower Left: %d" % towerData.Count
+	
+
+func _on_placeTowerButton_pressed(towerID):
 	towerIdToBePlaced = towerID
 
 func _input(event):
@@ -28,9 +70,14 @@ func _input(event):
 		hasSelectedTowerPosition = true
 
 func _physics_process(delta):
-	if hasSelectedTowerPosition && towerIdToBePlaced != Tower.INVALID && rayCast.is_colliding() :
-		var tower = getTower(towerIdToBePlaced)
-		tower.transform.origin = rayCast.get_collision_point()
-		get_tree().get_root().add_child(tower)
-		towerIdToBePlaced = Tower.INVALID
-		hasSelectedTowerPosition = false
+	if !hasSelectedTowerPosition || towerIdToBePlaced == Tower.INVALID:
+		return
+
+	if !rayCast.is_colliding() || "Area" in rayCast.get_collider().name:
+		messageLabel.text = "Invalid location!"
+		return
+
+	placeTower(getTower(towerIdToBePlaced), rayCast.get_collision_point())
+
+func isValidTowerPlacement(placementVec: Vector3) -> bool:
+	return true
