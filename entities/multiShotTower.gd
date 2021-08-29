8a0c581eebb21a2b8e574aba7713fd1b772ad918
wladi <wladi0097@googlemail.com>
2021-08-29 13:45:21 +0200

    increase bullet size and optimize direction

--- entities/multiShotTower.gd
+++ entities/multiShotTower.gd
@@ -1,6 +1,7 @@
 extends Spatial
 
 export var paused = true
+export var shootLookDown = -0.5
 onready var usedBullet: PackedScene = preload("res://entities/Bullet.tscn")
 onready var shootPoint1 = $ShootPoint1
 onready var shootPoint2 = $ShootPoint2
@@ -15,10 +16,10 @@ func _input(event):
 		shoot()
 
 func shoot():
-	shootDirection(shootPoint1, Vector3(1, 0, 0))
-	shootDirection(shootPoint2, Vector3(-1, 0, 0))
-	shootDirection(shootPoint3, Vector3(0, 0, -1))
-	shootDirection(shootPoint4, Vector3(0, 0, 1))
+	shootDirection(shootPoint1, Vector3(1, shootLookDown, 0))
+	shootDirection(shootPoint2, Vector3(-1, shootLookDown, 0))
+	shootDirection(shootPoint3, Vector3(0, shootLookDown, -1))
+	shootDirection(shootPoint4, Vector3(0, shootLookDown, 1))
 	
 func shootDirection(shootPoint, direction):
 	var bullet = usedBullet.instance().set_speed(5) \
